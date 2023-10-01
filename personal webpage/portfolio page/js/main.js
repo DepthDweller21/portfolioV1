354d9c182a2bcb2cddd931bb03de3a601ded4a72
Muhammad Haris <muhammadharis571@gmail.com>
2023-10-01 18:39:26 +0500

    changed fonts, added animations, added intro

--- personal webpage/portfolio page/js/main.js
+++ personal webpage/portfolio page/js/main.js
@@ -3,3 +3,29 @@ document.querySelector('.forexLaw').addEventListener('click',potato)
 function potato(){
     window.location.replace("https://forexlaw.netlify.app");
 }
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logoHeader');
+let logoSpan = document.querySelectorAll('.introLogo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{ setTimeout(()=>{
+        span.classList.add('active');
+        }, (idx + 1) * 300)
+        })
+    })
+});
+
+setTimeout(()=>{
+    logoSpan.forEach((span, idx)=>{
+    setTimeout(()=>{
+    span.classList.remove('active');
+    span.classList.add('fade');
+}, (idx + 1)*50)
+})
+}, 2000);
+
+setTimeout(()=>{
+intro.style.top = '-100vh'},2500);
